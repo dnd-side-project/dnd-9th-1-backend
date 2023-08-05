500df00fdb92d00d480c4c04d324acf65636b6ac
MingyeomKim <67851124+MingyeomKim@users.noreply.github.com>
2023-08-05 17:33:17 +0900

    ci: deploy.sh 의 docker 명령어에 sudo 권한 부여

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,27 +7,27 @@ EXIST_BLUE=$(sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.bl
 
 if [ -z "$EXIST_BLUE" ]; then
   echo "> blue up"
-  docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
+  sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
   BEFORE_COMPOSE_COLOR="green"
   AFTER_COMPOSE_COLOR="blue"
 else
   echo "> green up"
-  docdker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build
+  sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build
   BEFORE_COMPOSE_COLOR="blue"
   AFTER_COMPOSE_COLOR="green"
 fi
 
 sleep 10
 
-EXIST_AFTER=$(docker-compose -p ${DOCKER_APP_NAME}-${AFTER_COMPOSE_COLOR} -f docker-compose.${AFTER_COMPOSE_COLOR}.yml ps | grep UP)
+EXIST_AFTER=$(sudo docker-compose -p ${DOCKER_APP_NAME}-${AFTER_COMPOSE_COLOR} -f docker-compose.${AFTER_COMPOSE_COLOR}.yml ps | grep UP)
 
 if[ -n "$EXIST_AFTER"]; then
   echo "> nginx reload .. "
   NGINX_CONT=$(docker ps -q --filter ancestor=nginx)
-  docker exec -it "$NGINX_CONT" bash -c "cp /etc/nginx/nginx.${AFTER_COMPOSE_COLOR}.conf /etc/nginx/nginx.conf;nginx -s reload"
+  sudo docker exec -it "$NGINX_CONT" bash -c "cp /etc/nginx/nginx.${AFTER_COMPOSE_COLOR}.conf /etc/nginx/nginx.conf;nginx -s reload"
 
-  docker-compose -f docker-compose.${BEFORE_COMPOSE_COLOR}.yml down
-  docker rm $(docker ps -q -a -f "name=${BEFORE_COMPOSE_COLOR}")
+  sudo docker-compose -p ${DOCKER_APP_NAME}-${BEFORE_COMPOSE_COLOR} -f docker-compose.${BEFORE_COMPOSE_COLOR}.yml down
+  sudo docker rm $(docker ps -q -a -f "name=${BEFORE_COMPOSE_COLOR}")
   echo "$BEFORE_COMPOSE_COLOR down"
 fi
 

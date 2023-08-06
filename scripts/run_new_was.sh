9ba988916c6dfbc23d8f0ea4f59a8bf8d36e4496
MingyeomKim <67851124+MingyeomKim@users.noreply.github.com>
2023-08-06 22:41:26 +0900

    ci: run_new_was.sh 오류 수정

--- scripts/run_new_was.sh
+++ scripts/run_new_was.sh
@@ -1,27 +1,29 @@
-# run_new_was.sh
-
 #!/bin/bash
 
+# Parse port number from 'service_url.inc'
 CURRENT_PORT=$(cat /home/ubuntu/service_url.inc | grep -Po '[0-9]+' | tail -1)
 TARGET_PORT=0
 
 echo "> Current port of running WAS is ${CURRENT_PORT}."
 
+# Find target port to switch
 if [ ${CURRENT_PORT} -eq 8081 ]; then
-  TARGET_PORT=8082
+    TARGET_PORT=8082
 elif [ ${CURRENT_PORT} -eq 8082 ]; then
-  TARGET_PORT=8081
+    TARGET_PORT=8081
 else
-  echo "> No WAS is connected to nginx"
+    echo "> No WAS is connected to nginx"
 fi
 
+# query pid using the TCP protocol and using the port 'TARGET_PORT'
 TARGET_PID=$(lsof -Fp -i TCP:${TARGET_PORT} | grep -Po 'p[0-9]+' | grep -Po '[0-9]+')
 
 if [ ! -z ${TARGET_PID} ]; then
-  echo "> Kill WAS running at ${TARGET_PORT}."
-  sudo kill ${TARGET_PID}
+    echo "> Kill WAS running at ${TARGET_PORT}."
+    sudo kill ${TARGET_PID}
 fi
 
-nohup java -jar -Dserver.port=${TARGET_PORT} /home/ubuntu/playground-logging/build/libs/* > /home/ubuntu/nohup.out 2>&1 &
+# run jar file in background
+nohup java -jar -Dserver.port=${TARGET_PORT} /home/ubuntu/app/build/libs/* > /home/ubuntu/nohup.out 2>&1 &
 echo "> Now new WAS runs at ${TARGET_PORT}."
 exit 0

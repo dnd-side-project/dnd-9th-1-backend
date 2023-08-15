2b061bdfce5d1ad5955575139157fde040154cad
MingyeomKim <67851124+MingyeomKim@users.noreply.github.com>
2023-08-15 23:42:04 +0900

    docs: @Parameter 어노테이션으로  OAuthController 파라미터 설명 추가

--- src/main/java/com/backend/auth/presentation/OAuthController.java
+++ src/main/java/com/backend/auth/presentation/OAuthController.java
@@ -3,8 +3,8 @@ package com.backend.auth.presentation;
 import com.backend.auth.application.OAuthService;
 import com.backend.auth.presentation.dto.response.LoginResponse;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,15 +17,12 @@ public class OAuthController {
 
     private final OAuthService oauthService;
 
-    @Operation(summary = "소셜 로그인", description = "소셜 로그인 후 사용자 토큰 발급")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "소셜 로그인 성공"),
-            @ApiResponse(responseCode = "400", description = "잘못된 요청으로 인한 실패"),
-            @ApiResponse(responseCode = "401", description = "접근 권한 없음")
-    })
+    @Operation(summary = "소셜 로그인 API입니다.",
+            description = "카카오, 애플 서버에서 로그인한 사용자의 userId를 통해 access token과 refresh token을 반환합니다.")
     @PostMapping("/auth/{provider}")
-    public ResponseEntity<LoginResponse> generateAccessToken(@PathVariable String provider, @RequestParam String userId) {
+    public ResponseEntity<LoginResponse> generateAccessTokenAndRefreshToken(
+            @Parameter(description = ",kakao apple 중 현재 로그인하는 소셜 타입", in = ParameterIn.PATH) @PathVariable String provider,
+            @Parameter(description = "사용자 ID") @RequestParam String userId) {
         return ResponseEntity.ok(oauthService.login(provider, userId));
     }
-
 }

8de9f16b598c031c6acae4ae23714c098f885673
Seo Jemin <82302520+jemlog@users.noreply.github.com>
2023-08-20 10:51:50 +0900

    [Feature/#23] 하위 목표 모두 성공시 보상 제공 및 팝업 기능 구현 (#25)
    * feat: 상위 목표 d-day 기능 구현, 테스트 작성 (#6)
    
    - 상위 목표 종료 일자 필수 입력으로 확정 후, d-day 계산 로직 plan 엔티티내 생성 후 테스트
    - planService에 상위 목표 단건 조회 로직 추가
    
    * 간격 수정
    
    * feat: 보상 제공 기능 구현 (#23)
    
    * feat: 상위 목표 달성 시 팝업 기능 구현 (#23)
    
    - 랜덤으로 보석 지급 기능 구현
    - 상위 목표 리스트 조회 시 보석 enum도 조회 추가
    - 보석 지급과 함께 complete 상태의 상위 목표 개수 조회
    
    * test: 상위 목표 성공 시 기능 테스트 작성 (#23)
    
    * refactor: 상위 목표 개수 반환값 및 주석 수정

---
+++ src/main/java/com/backend/goal/domain/RewardType.java
@@ -0,0 +1,25 @@
+package com.backend.goal.domain;
+
+public enum RewardType {
+    BLUE_JEWEL_1,
+    BLUE_JEWEL_2,
+    BLUE_JEWEL_3,
+    BLUE_JEWEL_4,
+    BLUE_JEWEL_5,
+    PURPLE_JEWEL_1,
+    PURPLE_JEWEL_2,
+    PURPLE_JEWEL_3,
+    PURPLE_JEWEL_4,
+    PURPLE_JEWEL_5,
+    PINK_JEWEL_1,
+    PINK_JEWEL_2,
+    PINK_JEWEL_3,
+    PINK_JEWEL_4,
+    PINK_JEWEL_5,
+    GREEN_JEWEL_1,
+    GREEN_JEWEL_2,
+    GREEN_JEWEL_3,
+    GREEN_JEWEL_4,
+    GREEN_JEWEL_5;
+
+}

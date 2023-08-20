021fc6e2213300bbc8491f9fcacf36f92c06c58e
MingyeomKim <mingyum119@naver.com>
2023-08-20 12:01:41 +0900

    feat: 인증, 인가 로직 실패 시 Exception Handler 작성

---
+++ src/main/java/com/backend/auth/jwt/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,20 @@
+package com.backend.auth.jwt;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    // JWT 인증 실패 (Authentication Exception) 시 401 에러를 반환한다.
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+    }
+}

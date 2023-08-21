036259a5f195d293492e6ad70f0cb448fb3ff475
MingyeomKim <mingyum119@naver.com>
2023-08-21 22:56:11 +0900

    build: run_new_was.sh 스크립트 들여쓰기 수정

--- scripts/run_new_was.sh
+++ scripts/run_new_was.sh
@@ -19,8 +19,8 @@ fi
 TARGET_PID=$(lsof -Fp -i TCP:${TARGET_PORT} | grep -Po 'p[0-9]+' | grep -Po '[0-9]+')
 
 if [ ! -z ${TARGET_PID} ]; then
-    echo "> Kill WAS running at ${TARGET_PORT}."
-    sudo kill ${TARGET_PID}
+  echo "> Kill WAS running at ${TARGET_PORT}."
+  sudo kill ${TARGET_PID}
 fi
 
 # run jar file in background

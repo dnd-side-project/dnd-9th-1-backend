1aee7f761569a29be0cf99af916a482189c9807d
MingyeomKim <mingyum119@naver.com>
2023-08-21 20:34:54 +0900

    chore: swagger에 authorization 인증 받을 수 있도록 수정

--- src/main/java/com/backend/global/config/SwaggerConfig.java
+++ src/main/java/com/backend/global/config/SwaggerConfig.java
@@ -3,19 +3,32 @@ package com.backend.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
     @Bean
-    public OpenAPI openAPI(){
-        Info info = new Info()
-                .title("DND-9th-1 'Milestone' API Document")
-                .version("v0.0.1")
-                .description("DND 9기 1팀 'Milestone' 프로젝트의 API 명세서입니다.");
+    public OpenAPI openAPI() {
         return new OpenAPI()
-                .components(new Components())
-                .info(info);
+                .addServersItem(new Server().url("/"))
+                .info(
+                        new Info()
+                                .title("DND-9th-1 'Milestone' API Document")
+                                .description("DND 9기 1팀 'Milestone' 프로젝트의 API 명세서입니다.")
+                                .version("v0.0.1")
+                )
+                .components(
+                        new Components()
+                                .addSecuritySchemes(
+                                        "Authorization",
+                                        new SecurityScheme()
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("Bearer")
+                                                .bearerFormat("JWT")
+                                )
+                );
     }
 }

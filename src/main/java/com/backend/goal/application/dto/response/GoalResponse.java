bf9eeea8b1a716e4a3de97a172e4cc0edbe5b6c3
jemin <jemin03120111@gmail.com>
2023-08-26 01:11:51 +0900

    refactor: GoalResponse의 JsonFormat 수정

--- src/main/java/com/backend/goal/application/dto/response/GoalResponse.java
+++ src/main/java/com/backend/goal/application/dto/response/GoalResponse.java
@@ -2,10 +2,23 @@ package com.backend.goal.application.dto.response;
 
 
 import com.backend.goal.domain.Goal;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.time.LocalDate;
 
-public record GoalResponse(Long goalId, String title, LocalDate startDate, LocalDate endDate, Long dDay) {
+public record GoalResponse(
+
+        Long goalId,
+        String title,
+
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd", timezone = "Asia/Seoul")
+        LocalDate startDate,
+
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd", timezone = "Asia/Seoul")
+        LocalDate endDate,
+        Long dDay
+
+) {
 
         public static GoalResponse from(Goal goal, Long dDay)
         {

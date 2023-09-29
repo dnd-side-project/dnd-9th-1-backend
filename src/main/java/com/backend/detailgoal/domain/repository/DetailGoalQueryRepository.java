daf00fa9410a6ffa440c6fa24bbb075101be631e
Jemlog <82302520+jemlog@users.noreply.github.com>
2023-09-29 21:48:22 +0900

    채움함에 있는 상위 목표의 세부 목표만 알림이 보내지도록 수정 (#72)
    * chore: submodule 업데이트
    
    * fix: 채움함 이외의 하위 목표 조회 되는 현상 해결 (#71)
    
    * fix: 테스트 정상화
    
    - 인증 추가에 따라 테스트 코드의 변동사항들도 모두 반영
    - 차후 redis 연동 테스트는 testContainer를 통해 redis 실행할 예정
    
    * cicd: build.yml내 중복 테스트 제거
    
    * test: 빌드 실패로 인한 redis 제거 테스트
    
    * test: test db로 h2 추가
    
    * test: test db dialect 설정 변경
    
    * test: build.yml 서브모듈 접근 권한 추가
    
    * test: dialect 초기화
    
    * test: build.yml내 redis 실행 구문 복구
    
    * test: 리프레시 토큰 재발급 로직 수정
    
    - 내부에 있던 sleep 제거
    
    * chore: logback 추가
    
    * cicd: deploy.yml 수정

--- src/main/java/com/backend/detailgoal/domain/repository/DetailGoalQueryRepository.java
+++ src/main/java/com/backend/detailgoal/domain/repository/DetailGoalQueryRepository.java
@@ -2,6 +2,7 @@ package com.backend.detailgoal.domain.repository;
 
 import com.backend.detailgoal.application.dto.response.DetailGoalAlarmResponse;
 import com.backend.goal.domain.QGoal;
+import com.backend.goal.domain.enums.GoalStatus;
 import com.backend.member.domain.QMember;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -29,6 +30,7 @@ public class DetailGoalQueryRepository {
                 .leftJoin(goal).on(goal.id.eq(detailGoal.goalId))
                 .leftJoin(member).on(member.id.eq(goal.memberId))
                 .where(
+                        goal.goalStatus.eq(GoalStatus.PROCESS), // 채움함에 있는 상위 목표만 알림을 보낸다
                         detailGoal.isDeleted.isFalse(), // 삭제 되지 않은 것들만 조회
                         detailGoal.isCompleted.isFalse(), // 아직 완료되지 않은 것들 조회
                         detailGoal.alarmEnabled.isTrue(), // 알람을 허용한 하위 댓글 조회

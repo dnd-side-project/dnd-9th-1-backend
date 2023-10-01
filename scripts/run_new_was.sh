d0899456b6ecd83d24367acd128fc4baedc5260a
김민겸 <67851124+Mingyum-Kim@users.noreply.github.com>
2023-10-01 12:05:25 +0900

    [Feature/#79] 분리한 배포 환경을 적용하고 테스트 (#81)
    * refactor: 개발환경 분리되도록 워크플로우 수정
    
    * refactor: dev push 시 build/test 제외
    
    * feat: 배포 서버 별로 다른 profile로 실행하도록 run_new_was.sh 수정

--- scripts/run_new_was.sh
+++ scripts/run_new_was.sh
@@ -4,6 +4,7 @@ echo "> Start run_new_was.sh"
 
 # Parse port number from 'service_url.inc'
 CURRENT_PORT=$(cat /home/ubuntu/service_url.inc | grep -Po '[0-9]+' | tail -1)
+CURRENT_PROFILE=$(cat /home/ubuntu/profile.inc)
 TARGET_PORT=0
 
 echo "> Current port of running WAS is ${CURRENT_PORT}."
@@ -29,6 +30,7 @@ echo "> Kill WAS running at ${TARGET_PORT}."
 sudo kill $(sudo lsof -t -i:${TARGET_PORT})
 
 # run jar file in background
-nohup sudo java -jar -Duser.timezone="Asia/Seoul" -Dspring.profiles.active=dev -Dserver.port=${TARGET_PORT} /home/ubuntu/app/build/libs/backend-0.0.1-SNAPSHOT.jar > /home/ubuntu/nohup.out 2>&1 &
-echo "> Now new WAS runs at ${TARGET_PORT}."
+nohup sudo java -jar -Duser.timezone="Asia/Seoul" -Dspring.profiles.active=${CURRENT_PROFILE} -Dserver.port=${TARGET_PORT} /home/ubuntu/app/build/libs/backend-0.0.1-SNAPSHOT.jar > /home/ubuntu/nohup.out 2>&1 &
+echo "> Now new WAS runs at profile : ${CURRENT_PROFILE}."
+echo "> Now new WAS runs at port number : ${TARGET_PORT}."
 exit 0

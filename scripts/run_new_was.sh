6db0cd84b2db8c3fec10b048dd33a97eb266524c
MingyeomKim <67851124+MingyeomKim@users.noreply.github.com>
2023-10-06 20:02:47 +0900

    fix: run_new_was.sh 의 오타 수정

--- scripts/run_new_was.sh
+++ scripts/run_new_was.sh
@@ -13,7 +13,7 @@ echo "> Current port of running WAS is ${CURRENT_PORT}."
 if [ ${CURRENT_PORT} -eq 8081 ]; then
     TARGET_PORT=8082
 elif [ ${CURRENT_PORT} -eq 8082 ]; then
-    TARGET_PORT=8081ubu6
+    TARGET_PORT=8081
 else
     echo "> No WAS is connected to nginx"
 fi

19127a94669406ab81054930fad8a0fb6e99545d
MingyeomKim <67851124+MingyeomKim@users.noreply.github.com>
2023-11-23 00:08:29 +0900

    feat: prod 서버의 쉘 스크립트를 수정

--- scripts/prod/run_new_was.sh
+++ scripts/prod/run_new_was.sh
@@ -2,8 +2,8 @@
 
 echo "> Start run_new_was.sh"
 
-# Parse port number from 'service_url_prod.inc'
-CURRENT_PORT=$(cat /home/ubuntu/service_url_prod.inc | grep -Po '[0-9]+' | tail -1)
+# Parse port number from 'prod_url.inc'
+CURRENT_PORT=$(cat /home/ubuntu/prod_url.inc | grep -Po '[0-9]+' | tail -1)
 CURRENT_PROFILE=prod
 TARGET_PORT=0
 
@@ -23,7 +23,7 @@ echo "> Kill WAS running at ${TARGET_PORT}."
 sudo kill $(sudo lsof -t -i:${TARGET_PORT})
 
 # run jar file in background
-nohup sudo java -jar -Duser.timezone="Asia/Seoul" -Dspring.profiles.active=${CURRENT_PROFILE} -Dserver.port=${TARGET_PORT} /home/ubuntu/app/prod/build/libs/backend-0.0.1-SNAPSHOT.jar > /home/ubuntu/app/prod/nohup.out 2>&1 &
+nohup sudo java -jar -Duser.timezone="Asia/Seoul" -Dspring.profiles.active=${CURRENT_PROFILE} -Dserver.port=${TARGET_PORT} /home/ubuntu/prod/build/libs/backend-0.0.1-SNAPSHOT.jar > /home/ubuntu/prod/nohup.out 2>&1 &
 echo "> Now new WAS runs at profile : ${CURRENT_PROFILE}."
 echo "> Now new WAS runs at port number : ${TARGET_PORT}."
 exit 0
--- src/main/java/com/backend/global/config/SwaggerConfig.java
+++ src/main/java/com/backend/global/config/SwaggerConfig.java
@@ -9,8 +9,6 @@ import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Collections;
-
 @Configuration
 public class SwaggerConfig {
     @Bean

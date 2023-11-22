20222163b02428c6177fab35301fc09ff3fffc73
MingyeomKim <67851124+MingyeomKim@users.noreply.github.com>
2023-11-22 22:44:33 +0900

    feat: 배포 스크립트를 브랜치 별로 분리

---
+++ scripts/dev/run_new_was.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo "> Start run_new_was.sh"
+
+# Parse port number from 'service_url.inc'
+CURRENT_PORT=$(cat /home/ubuntu/service_url_dev.inc | grep -Po '[0-9]+' | tail -1)
+CURRENT_PROFILE=dev
+TARGET_PORT=0
+
+echo "> Current port of running WAS is ${CURRENT_PORT}."
+
+# Find target port to switch
+if [ ${CURRENT_PORT} -eq 8083 ]; then
+    TARGET_PORT=8084
+elif [ ${CURRENT_PORT} -eq 8084 ]; then
+    TARGET_PORT=8083
+else
+    echo "> No WAS is connected to nginx"
+fi
+
+# query pid using the TCP protocol and using the port 'TARGET_PORT'
+# TARGET_PID=$(lsof -Fp -i TCP:${TARGET_PORT} | grep -Po 'p[0-9]+' | grep -Po '[0-9]+')
+
+# if [ ! -z ${TARGET_PID} ]; then
+#  echo "> Kill WAS running at ${TARGET_PORT}."
+#  sudo kill ${TARGET_PID}
+# fi
+
+echo "> Kill WAS running at ${TARGET_PORT}."
+sudo kill $(sudo lsof -t -i:${TARGET_PORT})
+
+# run jar file in background
+nohup sudo java -jar -Duser.timezone="Asia/Seoul" -Dspring.profiles.active=${CURRENT_PROFILE} -Dserver.port=${TARGET_PORT} /home/ubuntu/app/dev/build/libs/backend-0.0.1-SNAPSHOT.jar > /home/ubuntu/nohup.out 2>&1 &
+echo "> Now new WAS runs at profile : ${CURRENT_PROFILE}."
+echo "> Now new WAS runs at port number : ${TARGET_PORT}."
+exit 0
---
+++ scripts/dev/switch.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Crawl current connected port of WAS
+CURRENT_PORT=$(cat /home/ubuntu/service_url_dev.inc  | grep -Po '[0-9]+' | tail -1)
+TARGET_PORT=0
+
+echo "> Nginx currently proxies to ${CURRENT_PORT}."
+
+# Toggle port number
+if [ ${CURRENT_PORT} -eq 8083 ]; then
+    TARGET_PORT=8084
+elif [ ${CURRENT_PORT} -eq 8084 ]; then
+    TARGET_PORT=8083
+else
+    echo "> No WAS is connected to nginx"
+    exit 1
+fi
+
+# Change proxying port into target port
+echo "set \$service_url http://127.0.0.1:${TARGET_PORT};" | tee /home/ubuntu/service_url_dev.inc
+
+echo "> Now nginx proxies to ${TARGET_PORT}."
+
+# Reload nginx
+sudo service nginx reload
+
+echo "> Nginx reloaded."
